cc3e72f84728df004cfa0bb28af97bb41a66c697
elaubinger <elaubinger@yahoo.com>
2016-02-08 01:40:44 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -82,8 +82,8 @@
             this.pacman.body.setSize(16, 16, 0, 0);
             
             //Add enemy to game
-            this.car = this.add.sprite(0, 0, 'car', 0);
-            this.physics.arcade.enable(this.car);
+            this.car = this.add.sprite(0, 2, 'car', 0);
+            //this.physics.arcade.enable(this.car);
 
             this.cursors = this.input.keyboard.createCursorKeys();
 
